beecddaca91e7221a571210a16fec4d5c6fcf167
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2019-05-21 18:39:02 +0200

    nicola: added support to xenomai3

--- cmake/Modules/FindXenomai.cmake
+++ cmake/Modules/FindXenomai.cmake
@@ -1,186 +1,155 @@
-# (C) Copyright 2005-2014 Johns Hopkins University (JHU), All Rights
-# Reserved.
-#
-# --- begin cisst license - do not edit ---
+################################################################################
+# Copyright 2019 Antoine HOARAU <antoine [at] fuzzylogicrobotics.com>
 # 
-# This software is provided "as is" under an open source license, with
-# no warranty.  The complete license can be found in license.txt and
-# http://www.cisst.org/cisst/license.txt.
-# 
-# --- end cisst license ---
+# CMake script for finding the XENOMAI 2 native or XENOMAI 3 alchemy/posix/rtdm skin.
+# If the optional XENOMAI_ROOT_DIR environment variable exists, header files and
+# libraries will be searched in the XENOMAI_ROOT_DIR/include and XENOMAI_ROOT_DIR/lib
+# directories, respectively. Otherwise the default CMake search process will be
+# used.
 #
-# CMake script for finding Xenomai
+# This script creates the following variables for each skin.
 #
-# This will find Xenomai on Linux systems and define flags for each of the
-# supported Xenomai "skins". The current supported skins are:
+#  Native/Alchemy skin :
 #
-# - NATIVE
-# - POSIX
-# 
-# Input variables:
-# 
-# - ${Xenomai_ROOT_DIR} (optional): Used as a hint to find the Xenomai root dir
-# - $ENV{XENOMAI_ROOT_DIR} (optional): Used as a hint to find the Xenomai root dir
+#  XENOMAI_FOUND: Boolean that indicates if the package was found
+#  XENOMAI_INCLUDE_DIRS: Paths to the necessary header files
+#  XENOMAI_LIBRARIES: Package libraries
+#  XENOMAI_CFLAGS: Package cflags for additional parsing
+#  XENOMAI_LDFLAGS: Package linker flags for additional parsing
+#  XENOMAI_COMPILE_DEFINITIONS: Package compile flags
 #
-# Cache variables:
+#  And it also set the XENOMAI_POSIX_* and XENOMAI_RTDM_* variables.
 #
-# - Xenomai_ROOT_DIR
-# - Xenomai_INCLUDE_DIRS
+#  Posix Interface : 
 #
-# Output Variables:
+#  XENOMAI_POSIX_FOUND: Boolean that indicates if the package was found
+#  XENOMAI_POSIX_INCLUDE_DIRS: Paths to the necessary header files
+#  XENOMAI_POSIX_LIBRARIES: Package libraries
+#  XENOMAI_POSIX_CFLAGS: Package cflags for additional parsing
+#  XENOMAI_POSIX_LDFLAGS: Package linker flags for additional parsing
+#  XENOMAI_POSIX_COMPILE_DEFINITIONS: Package compile flags
 #
-# - Xenomai_FOUND: Boolean that indicates if the package was found. See the
-#   Xenomai_*_FOUND variables below for individual skins.
-# - Xenomai_VERSION: major.minor.patch Xenomai version string
-# - Xenomai_XENO_CONFIG: Path to xeno-config program
+#  RTDM Interface :
 #
-# - Individual library variables:
-#   - Xenomai_LIBRARY_XENOMAI
-#   - Xenomai_LIBRARY_NATIVE
-#   - Xenomai_LIBRARY_PTHREAD_RT
-#   - Xenomai_LIBRARY_RTDM
-#   - Xenomai_LIBRARY_RTDK ( this will be empty, deprecated after Xenomai 2.6.0)
+#  XENOMAI_RTDM_FOUND: Boolean that indicates if the package was found
+#  XENOMAI_RTDM_INCLUDE_DIRS: Paths to the necessary header files
+#  XENOMAI_RTDM_LIBRARIES: Package libraries
+#  XENOMAI_RTDM_CFLAGS: Package cflags for additional parsing
+#  XENOMAI_RTDM_LDFLAGS: Package linker flags for additional parsing
+#  XENOMAI_RTDM_COMPILE_DEFINITIONS: Package compile flags
 #
-# - Native Flags:
-#   - Xenomai_NATIVE_FOUND: Boolean that indicates if the native skin was found
-#   - Xenomai_NATIVE_DEFINITIONS
-#   - Xenomai_NATIVE_INCLUDE_DIRS
-#   - Xenomai_NATIVE_LIBRARY_DIRS
-#   - Xenomai_NATIVE_LIBRARIES
-#   - Xenomai_NATIVE_LDFLAGS
-# 
-# - POSIX Flags:
-#   - Xenomai_POSIX_FOUND: Boolean that indicates if the posix skin was found
-#   - Xenomai_POSIX_DEFINITIONS
-#   - Xenomai_POSIX_INCLUDE_DIRS
-#   - Xenomai_POSIX_LIBRARY_DIRS
-#   - Xenomai_POSIX_LIBRARIES
-#   - Xenomai_POSIX_LDFLAGS
-#
-# - RTDM Flags:
-#   - Xenomai_RTDM_FOUND: Boolean that indicates if the RTDM skin was found
-#   - Xenomai_RTDM_DEFINITIONS
-#   - Xenomai_RTDM_INCLUDE_DIRS
-#   - Xenomai_RTDM_LIBRARY_DIRS
-#   - Xenomai_RTDM_LIBRARIES
-#   - Xenomai_RTDM_LDFLAGS
-
-if( UNIX )
-
-  # Get hint from environment variable (if any)
-  if(NOT $ENV{XENOMAI_ROOT_DIR} STREQUAL "")
-    set(XENOMAI_ROOT_DIR $ENV{XENOMAI_ROOT_DIR} CACHE PATH "Xenomai base directory location (optional, used for nonstandard installation paths)" FORCE)
-    mark_as_advanced(XENOMAI_ROOT_DIR)
-  endif()
-
-  # set the search paths
-  set( Xenomai_SEARCH_PATH /usr/local /usr $ENV{XENOMAI_ROOT_DIR} ${Xenomai_ROOT_DIR})
-  
-  # find xeno_config.h
-  find_path( Xenomai_INCLUDE_DIR
-    xeno_config.h 
-    PATHS ${Xenomai_SEARCH_PATH} 
-    PATH_SUFFIXES xenomai include xenomai/include include/xenomai
-    )
-
-  # did we find xeno_config.h?
-  if(Xenomai_INCLUDE_DIR) 
-    MESSAGE(STATUS "xenomai found: \"${Xenomai_INCLUDE_DIR}\"")
-    
-    # set the root directory
-    if( "${Xenomai_INCLUDE_DIR}" MATCHES "/usr/include/xenomai" )
-      # on ubuntu linux, xenomai install is not rooted to a single dir
-      set( Xenomai_ROOT_DIR /usr CACHE PATH "The Xenomai FHS root")
-      set( Xenomai_INCLUDE_POSIX_DIR ${Xenomai_INCLUDE_DIR}/posix )
-    else()
-      # elsewhere, xenomai install is packaged
-      get_filename_component(Xenomai_ROOT_DIR ${Xenomai_INCLUDE_DIR} PATH CACHE)
-      set( Xenomai_INCLUDE_POSIX_DIR ${Xenomai_ROOT_DIR}/include/posix )
-    endif()
-
-    # Find xeno-config
-    find_program(Xenomai_XENO_CONFIG NAMES xeno-config  PATHS ${Xenomai_ROOT_DIR}/bin NO_DEFAULT_PATH)
-
-    # get xenomai version
-    execute_process(COMMAND ${Xenomai_XENO_CONFIG} --version OUTPUT_VARIABLE Xenomai_VERSION)
-    
-    # find the xenomai pthread library
-    find_library( Xenomai_LIBRARY_NATIVE     native     ${Xenomai_ROOT_DIR}/lib )
-    find_library( Xenomai_LIBRARY_XENOMAI    xenomai    ${Xenomai_ROOT_DIR}/lib )
-    find_library( Xenomai_LIBRARY_PTHREAD_RT pthread_rt ${Xenomai_ROOT_DIR}/lib )
-    find_library( Xenomai_LIBRARY_RTDM       rtdm       ${Xenomai_ROOT_DIR}/lib )
-
-    # In 2.6.0 RTDK was merged into the main xenomai library
-    if(Xenomai_VERSION VERSION_GREATER 2.6.0)
-      set(Xenomai_LIBRARY_RTDK_FOUND ${Xenomai_LIBRARY_XENOMAI_FOUND})
-      set(Xenomai_LIBRARY_RTDK ${Xenomai_LIBRARY_XENOMAI})
-    else()
-      find_library( Xenomai_LIBRARY_RTDK rtdk ${Xenomai_ROOT_DIR}/lib )
-    endif()
-
-    # Xenomai libraries for each skin
-    set(Xenomai_NATIVE_LIBRARIES ${Xenomai_LIBRARY_NATIVE} ${Xenomai_LIBRARY_XENOMAI} pthread)
-    set(Xenomai_POSIX_LIBRARIES ${Xenomai_LIBRARY_PTHREAD_RT} ${Xenomai_LIBRARY_XENOMAI} pthread rt)
-    set(Xenomai_RTDM_LIBRARIES ${Xenomai_LIBRARY_RTDM} ${Xenomai_LIBRARY_XENOMAI} pthread rt)
-
-    # Xenomai LDFLAGS for each skin
-    set(Xenomai_NATIVE_LDFLAGS "")
-    set(Xenomai_POSIX_LDFLAGS "-Wl,@${Xenomai_ROOT_DIR}/lib/posix.wrappers")
-    set(Xenomai_RTDM_LDFLAGS "")
-
-    # Xenomai compiler definitions for each supported skin
-    set(Xenomai_NATIVE_DEFINITIONS -D_GNU_SOURCE -D_REENTRANT -D__XENO__)
-    set(Xenomai_POSIX_DEFINITIONS ${Xenomai_NATIVE_DEFINITIONS})
-    set(Xenomai_RTDM_DEFINITIONS ${Xenomai_NATIVE_DEFINITIONS})
-
-    # Xenomai library dirs for each skin
-    set( Xenomai_NATIVE_LIBRARY_DIRS ${Xenomai_ROOT_DIR}/lib )
-    set( Xenomai_POSIX_LIBRARY_DIRS ${Xenomai_NATIVE_LIBRARY_DIRS} )
-    set( Xenomai_RTDM_LIBRARY_DIRS ${Xenomai_NATIVE_LIBRARY_DIRS} )
-
-    # Xenomai library dirs for each skin
-    set( Xenomai_NATIVE_INCLUDE_DIRS ${Xenomai_INCLUDE_DIR} )
-    set( Xenomai_POSIX_INCLUDE_DIRS ${Xenomai_INCLUDE_DIR} ${Xenomai_INCLUDE_POSIX_DIR} )
-    set( Xenomai_RTDM_INCLUDE_DIRS ${Xenomai_INCLUDE_DIR} )
-
-    # Compatibility
-    set( Xenomai_LIBRARIES 
-      ${Xenomai_LIBRARY_XENOMAI}
-      ${Xenomai_LIBRARY_NATIVE}
-      ${Xenomai_LIBRARY_PTHREAD_RT}
-      ${Xenomai_LIBRARY_RTDM}
-      ${Xenomai_LIBRARY_RTDK}
-      )
-
-  else( )
-    MESSAGE(STATUS "Xenomai NOT found in paths: (${Xenomai_SEARCH_PATH})")
-  endif( )
-
-endif( UNIX )
+#  NOTE: You still need FindRTnet.cmake for rtnet support on xenomai 2.x
+################################################################################
 
 include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(Xenomai DEFAULT_MSG
-  Xenomai_ROOT_DIR
-  Xenomai_INCLUDE_DIR
-  Xenomai_LIBRARY_XENOMAI
-  Xenomai_LIBRARY_RTDK
-  )
-  
-set(Xenomai_FOUND ${XENOMAI_FOUND}) # Set appropriately cased variable
+# Get hint from environment variable (if any)
+if(NOT $ENV{XENOMAI_ROOT_DIR} STREQUAL "")
+    set(XENOMAI_ROOT_DIR $ENV{XENOMAI_ROOT_DIR} CACHE PATH "Xenomai base directory location (optional, used for nonstandard installation paths)" FORCE)
+    mark_as_advanced(XENOMAI_ROOT_DIR)
+endif()
 
-if(Xenomai_LIBRARY_XENOMAI AND Xenomai_LIBRARY_NATIVE) 
-  message(STATUS "Xenomai Native skin found")
-  set(Xenomai_NATIVE_FOUND True)
+message("XENOMAI_ROOT_DIR: ${XENOMAI_ROOT_DIR}")
+# Find headers and libraries
+if(XENOMAI_ROOT_DIR)
+    # Use location specified by environment variable
+    find_program(XENOMAI_XENO_CONFIG NAMES xeno-config  PATHS ${XENOMAI_ROOT_DIR}/bin NO_DEFAULT_PATH)
+else()
+    # Use default CMake search process
+    find_program(XENOMAI_XENO_CONFIG NAMES xeno-config )
 endif()
-  
-if(Xenomai_LIBRARY_XENOMAI AND Xenomai_LIBRARY_PTHREAD_RT) 
-  message(STATUS "Xenomai POSIX skin found")
-  set(Xenomai_POSIX_FOUND True)
+
+mark_as_advanced(XENOMAI_XENO_CONFIG)
+
+function(find_xeno_skin_variables prefix skin_name)
+    
+    execute_process(COMMAND ${XENOMAI_XENO_CONFIG} --skin=${skin_name} --ldflags ${XENO_CONFIG_LDFLAGS_EXTRA_ARGS}
+                    OUTPUT_VARIABLE ${prefix}_LDFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE
+                    ERROR_VARIABLE ${prefix}_LDFLAGS_ERROR)
+    execute_process(COMMAND ${XENOMAI_XENO_CONFIG} --skin=${skin_name} --cflags
+                    OUTPUT_VARIABLE ${prefix}_CFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE
+                    ERROR_VARIABLE ${prefix}_CFLAGS_ERROR)
+
+    if(${prefix}_LDFLAGS_ERROR)
+        message(FATAL_ERROR "Could not determine ldflags with command ${XENOMAI_XENO_CONFIG} --skin=${skin_name} --ldflags ${XENO_CONFIG_LDFLAGS_EXTRA_ARGS}")
+    endif()
+
+    if(${prefix}_CFLAGS_ERROR)
+        message(FATAL_ERROR "Could not determine cflags with command ${XENOMAI_XENO_CONFIG} --skin=${skin_name} --cflags")
+    endif()
+
+    set(${prefix}_FOUND TRUE)
+
+    if(NOT ${${prefix}_LDFLAGS} STREQUAL "")
+        string(REGEX MATCHALL "-L([^ ]+)|-l([^ ]+)|.*bootstrap(-pic)?.o" ${prefix}_LIBRARY ${${prefix}_LDFLAGS})
+    else()
+        set(${prefix}_LIBRARY "")
+    endif()
+    string(REGEX MATCHALL "-I([^ ]+)" ${prefix}_INCLUDE_DIR ${${prefix}_CFLAGS})
+    string(REGEX MATCHALL "-D([^ ]+)" ${prefix}_COMPILE_DEFINITIONS ${${prefix}_CFLAGS})
+    string(REPLACE "-I" ";" ${prefix}_INCLUDE_DIR ${${prefix}_INCLUDE_DIR})
+
+    message(STATUS "Xenomai ${XENOMAI_VERSION} ${skin_name} skin
+    libs    : ${${prefix}_LIBRARY}
+    include : ${${prefix}_INCLUDE_DIR}
+    ldflags : ${${prefix}_LDFLAGS}
+    cflags  : ${${prefix}_CFLAGS}
+    ")
+
+    set(${prefix}_INCLUDE_DIRS ${${prefix}_INCLUDE_DIR} CACHE INTERNAL "")
+    set(${prefix}_LIBRARIES ${${prefix}_LIBRARY} CACHE INTERNAL "")
+    set(${prefix}_COMPILE_DEFINITIONS ${${prefix}_COMPILE_DEFINITIONS} CACHE INTERNAL "")
+    set(${prefix}_LDFLAGS ${${prefix}_LDFLAGS} CACHE INTERNAL "")
+    set(${prefix}_CFLAGS ${${prefix}_CFLAGS} CACHE INTERNAL "")
+    set(${prefix}_FOUND ${${prefix}_FOUND} CACHE INTERNAL "")
+
+    mark_as_advanced(${prefix}_LIBRARIES ${prefix}_INCLUDE_DIRS ${prefix}_COMPILE_DEFINITIONS ${prefix}_CFLAGS ${prefix}_LDFLAGS)
+endfunction()
+
+function(handle_standard_args prefix)
+    find_package_handle_standard_args(${prefix} DEFAULT_MSG ${prefix}_LIBRARIES ${prefix}_INCLUDE_DIRS ${prefix}_COMPILE_DEFINITIONS ${prefix}_CFLAGS ${prefix}_LDFLAGS)
+endfunction()
+
+if(XENOMAI_XENO_CONFIG)
+    # Detect Xenomai version
+    execute_process(COMMAND ${XENOMAI_XENO_CONFIG} --version OUTPUT_VARIABLE XENOMAI_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+    string(REPLACE "." ";" XENOMAI_VERSION_LIST ${XENOMAI_VERSION} )
+    list(GET XENOMAI_VERSION_LIST 0 XENOMAI_VERSION_MAJOR)
+    list(GET XENOMAI_VERSION_LIST 1 XENOMAI_VERSION_MINOR)
+    #list(GET XENOMAI_VERSION_LIST 2 XENOMAI_VERSION_PATCH)
+
+    # Here we have xeno-config
+    if(${XENOMAI_VERSION_MAJOR} EQUAL 2)
+        set(XENOMAI_SKIN_NAME   native)
+    endif()
+
+    if(${XENOMAI_VERSION_MAJOR} EQUAL 3)
+        set(XENOMAI_SKIN_NAME   alchemy)
+        # NOTE: --auto-init-solib bootstraps xenomai_init()
+        set(XENO_CONFIG_LDFLAGS_EXTRA_ARGS "--auto-init-solib")
+    endif()
+
+    if(NOT XENOMAI_SKIN_NAME)
+        message(FATAL_ERROR "Only Xenomai 2.x and 3.x are supported, your version is ${XENOMAI_VERSION}")
+    endif()
+
+    find_xeno_skin_variables(XENOMAI ${XENOMAI_SKIN_NAME})
+    find_xeno_skin_variables(XENOMAI_POSIX posix)
+    find_xeno_skin_variables(XENOMAI_RTDM rtdm)
+else()
+    set(XENOMAI_FOUND FALSE)
+    set(XENOMAI_POSIX_FOUND FALSE)
+    set(XENOMAI_RTDM_FOUND FALSE)
 endif()
-  
-if(Xenomai_LIBRARY_XENOMAI AND Xenomai_LIBRARY_RTDM) 
-  message(STATUS "Xenomai RTDM skin found")
-  set(Xenomai_RTDM_FOUND True)
+
+if(Xenomai_FIND_QUIETLY)
+    set(XENOMAI_FIND_QUIETLY True)
+    set(XENOMAI_POSIX_FIND_QUIETLY True)
+    set(XENOMAI_RTDM_FIND_QUIETLY True)
 endif()
-  
+
+find_package_handle_standard_args(Xenomai DEFAULT_MSG XENOMAI_XENO_CONFIG)
+
+handle_standard_args(XENOMAI)
+handle_standard_args(XENOMAI_POSIX)
+handle_standard_args(XENOMAI_RTDM)
